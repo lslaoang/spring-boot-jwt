eb29249a757f2b24305ac5ee227c9d260bef93a6
Laurel Lao Ang <laurellaoang@gmail.com>
2022-03-08 14:20:31 +0800

    Added customized response.

--- src/main/java/com/javainuse/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/javainuse/config/JwtAuthenticationEntryPoint.java
@@ -6,6 +6,10 @@ import java.io.Serializable;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -19,6 +23,19 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException {
 
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+		response.setStatus(401);
+		response.getWriter().write("\"status\" : \"401 \"\n" +
+				"\"message\" : \"Malformed token \"");
+
+	}
+
+	String error = "Expired or invalid JWT token";
+	//ApiError errorResponse = new ApiError(new Date(),500, HttpStatus.INTERNAL_SERVER_ERROR, error, ex.getLocalizedMessage(), httpServletRequest.getRequestURL().toString());
+	private String convertObjectToJson(Object object) throws JsonProcessingException {
+		if (object == null) {
+			return null;
+		}
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.writeValueAsString(object);
 	}
 }
